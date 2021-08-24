1361637bab791bfa292aee2fd22ed646c1109ebc
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-24 06:53:12 -0700

    Update pyrogram.py

--- EzilaXBotV/Best_Of_EzilaXBotV/pyrogram.py
+++ EzilaXBotV/Best_Of_EzilaXBotV/pyrogram.py
@@ -1,4 +1,4 @@
-# This file is part of EzilaXBot (Telegram Bot)
+# This file is part of EzilaXXBot (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
