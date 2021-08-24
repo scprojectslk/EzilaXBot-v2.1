e15690d7a41f03d8afa8b4b6aace9109e3625d9c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-24 07:47:47 -0700

    Update logomaker.py

--- EzilaXBotV/modules/logomaker.py
+++ EzilaXBotV/modules/logomaker.py
@@ -181,6 +181,44 @@ async def lego(event):
  except Exception as e:
    await event.reply(f'Error Go to Help From  @Ezila_Support, {e}')
 
+
+@register(pattern="^/pandalogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply("Creating your logo...wait!")
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./EzilaXBotV/resources/pandalogo.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "white"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./EzilaXBotV/resources/Maghrib.ttf", 1000)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=0, stroke_fill="yellow")
+    fname2 = "EzilaXBotVLogo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @EzilaXBot ❤️")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Go to Help From  @Ezila_Support, {e}')
+
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
@@ -191,5 +229,6 @@ __help__ = """
  ❍ /logo text :  Create your logo with your name
  ❍ /rlogo text :  Create your logo Bigger Than `logo`
  ❍ /wlogo text :  Create your logo with your name
+  ❍ /wlogo text :  Create your logo with your name
  """
 __mod_name__ = "Logo Maker"
